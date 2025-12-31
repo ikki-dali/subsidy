33324544ffb413217d87fb554e60129441afda45
ikki-dali <yamamotoikki@forestdali.biz>
2025-12-31 15:45:13 +0900

    fix: ファビコンのバランス調整（円を大きく、文字を中央に）

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -25,24 +25,24 @@ export default function Icon() {
         {/* 虫眼鏡の円 */}
         <div
           style={{
-            width: 22,
-            height: 22,
+            width: 24,
+            height: 24,
             borderRadius: '50%',
-            border: '3px solid #1d6bb3',
+            border: '2.5px solid #1d6bb3',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
             position: 'absolute',
-            top: 2,
-            left: 2,
+            top: 1,
+            left: 1,
           }}
         >
           <span
             style={{
-              fontSize: 11,
+              fontSize: 13,
               fontWeight: 'bold',
               color: '#1d6bb3',
-              fontFamily: 'sans-serif',
+              fontFamily: 'Arial, sans-serif',
             }}
           >
             助
@@ -51,13 +51,13 @@ export default function Icon() {
         {/* 虫眼鏡の柄 */}
         <div
           style={{
-            width: 10,
-            height: 4,
+            width: 9,
+            height: 3,
             background: '#1d6bb3',
             borderRadius: 2,
             position: 'absolute',
-            bottom: 3,
-            right: 1,
+            bottom: 2,
+            right: 2,
             transform: 'rotate(45deg)',
           }}
         />
